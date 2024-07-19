f963b4b57a4552374585373e6e835b86a1287a50
Luke McAlpine <lukedmcalpine@gmail.com>
2024-07-18 21:09:53 -0700

    Convert to fixed

--- fixed.c
+++ fixed.c
@@ -5,12 +5,20 @@ int main()
 {
 
     // multiply by FRACUNIT to convert to fixed point
-    int a = 0.5*FRACUNIT;
-    int b = 3.75*FRACUNIT;
-    int sum = a + b;
+    // int a = 0.5*FRACUNIT;
+    // int b = 3.75*FRACUNIT;
+    fixed_t a = ConvertToFixed(0.5);
+    fixed_t b = ConvertToFixed(3.75);
+    fixed_t sum = a + b;
 
-    
+    fixed_t c = ConvertToFixed(2.5);
+    fixed_t d = c + ConvertToFixed(2);
 
+    // 0.25
+    fixed_t e = c / 10;
+}
 
-
+fixed_t ConvertToFixed(float num)
+{
+    return num * FRACUNIT;
 }
--- fixed.h
+++ fixed.h
@@ -1,8 +1,10 @@
 #define FRACBITS 16
-#define FRACUNIT (1<<FRACBITS)
+#define FRACUNIT (1<<FRACBITS) // 65536
 
 typedef int fixed_t;
 
 fixed_t FixedMul(fixed_t a, fixed_t b);
 fixed_t FixedDiv(fixed_t a, fixed_t b);
 fixed_t FixedDiv2(fixed_t a, fixed_t b);
+
+fixed_t ConvertToFixed(float num);

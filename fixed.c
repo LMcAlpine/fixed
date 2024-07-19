eb8f9bc67dd44f9844c370dd82e895156713c1cf
Luke McAlpine <lukedmcalpine@gmail.com>
2024-07-18 20:11:34 -0700

    initial commit

---
+++ fixed.c
@@ -0,0 +1,16 @@
+#include "fixed.h"
+#include <stdio.h>
+
+int main()
+{
+
+    // multiply by FRACUNIT to convert to fixed point
+    int a = 0.5*FRACUNIT;
+    int b = 3.75*FRACUNIT;
+    int sum = a + b;
+
+    
+
+
+
+}
---
+++ fixed.h
@@ -0,0 +1,8 @@
+#define FRACBITS 16
+#define FRACUNIT (1<<FRACBITS)
+
+typedef int fixed_t;
+
+fixed_t FixedMul(fixed_t a, fixed_t b);
+fixed_t FixedDiv(fixed_t a, fixed_t b);
+fixed_t FixedDiv2(fixed_t a, fixed_t b);
